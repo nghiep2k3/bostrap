b0d421220f6475f09c064a13d0d875fbd6ba4038
Nguyễn Nghiệp <nghiep2k3>
2022-11-17 14:08:21 +0700

    uptodate

---
+++ slick/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.Member').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        // infinite: true,
+        // arrows: true,
+        // draggable:false,
+        prevArrow:
+        `<button type='button' class='slick-prev pull-left'>
+            <img src="https://cdn-icons-png.flaticon.com/512/507/507257.png"></img>
+        </button>`,
+
+        nextArrow:
+        `<button type='button' class='slick-next pull-right'>
+        <img src="https://cdn-icons-png.flaticon.com/512/271/271226.png"></img>
+        </button>`,
+
+        responsive: [
+            // {
+            //   breakpoint: 1025,
+            //   settings: {
+            //     slidesToShow: 3,
+            //   }
+            // },
+
+            {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 3,
+                  arrows: false,
+                }
+            },
+        ]
+      });
+
+    
+    
+  });
